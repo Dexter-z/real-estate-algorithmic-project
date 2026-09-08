63d15ee3f0c5a5886bb1be99e887744c9885d612
Adaeze-2 <adaezeha2@gmail.com>
2026-09-08 10:46:45 +0100

    updated website design

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-function = getElementByid
+const menuButton = document.getElementById("menuButton");
+const navlinks = document.getElementById("navlinks");
+menuButton. addEventListener("click") , function() {
+    navlinks. classList.toggle("active");
+} 
+const links =document.querySelectorAll("navlinks a");{
+    links.addEventListener("click"), function(){
+        navlinks.classList.remove("active");
+    }
+}
